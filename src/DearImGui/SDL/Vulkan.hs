d2eaaa6226f8b562c5bf2e942ced83484f7f2fee
sheaf <sam.derbyshire@gmail.com>
2021-01-24 22:29:16 +0100

    Vulkan backend

---
+++ src/DearImGui/SDL/Vulkan.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+{-|
+Module: DearImGui.SDL.Vulkan
+
+Initialising the Vulkan backend for Dear ImGui using SDL2.
+-}
+
+module DearImGui.SDL.Vulkan
+  ( sdl2InitForVulkan )
+  where
+
+-- inline-c
+import qualified Language.C.Inline as C
+
+-- inline-c-cpp
+import qualified Language.C.Inline.Cpp as Cpp
+
+-- sdl2
+import SDL.Internal.Types
+  ( Window(..) )
+
+-- transformers
+import Control.Monad.IO.Class ( MonadIO, liftIO )
+
+
+C.context Cpp.cppCtx
+C.include "imgui.h"
+C.include "backends/imgui_impl_vulkan.h"
+C.include "backends/imgui_impl_sdl.h"
+C.include "SDL.h"
+C.include "SDL_vulkan.h"
+Cpp.using "namespace ImGui"
+
+
+-- | Wraps @ImGui_ImplSDL2_InitForVulkan@.
+sdl2InitForVulkan :: MonadIO m => Window -> m Bool
+sdl2InitForVulkan (Window windowPtr) = liftIO do
+  ( 0 /= ) <$> [C.exp| bool { ImGui_ImplSDL2_InitForVulkan((SDL_Window*)$(void* windowPtr)) } |]
---
+++ src/DearImGui/Vulkan/Types.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module DearImGui.Vulkan.Types
+  ( vulkanCtx )
+  where
+
+-- containers
+import qualified Data.Map.Strict as Map
+  ( fromList )
+
+-- inline-c
+import qualified Language.C.Inline.Context as C
+import qualified Language.C.Types          as C
+
+-- vulkan
+import qualified Vulkan
+
+vulkanTypesTable :: C.TypesTable
+vulkanTypesTable = Map.fromList
+  [ ( C.TypeName "VkAllocationCallbacks", [t| Vulkan.AllocationCallbacks |] )
+  , ( C.TypeName "VkCommandBuffer_T"    , [t| Vulkan.CommandBuffer_T     |] )
+  , ( C.TypeName "VkDescriptorPool"     , [t| Vulkan.DescriptorPool      |] )
+  , ( C.TypeName "VkDevice_T"           , [t| Vulkan.Device_T            |] )
+  , ( C.TypeName "VkInstance_T"         , [t| Vulkan.Instance_T          |] )
+  , ( C.TypeName "VkPhysicalDevice_T"   , [t| Vulkan.PhysicalDevice_T    |] )
+  , ( C.TypeName "VkPipeline"           , [t| Vulkan.Pipeline            |] )
+  , ( C.TypeName "VkPipelineCache"      , [t| Vulkan.PipelineCache       |] )
+  , ( C.TypeName "VkQueue_T"            , [t| Vulkan.Queue_T             |] )
+  , ( C.TypeName "VkRenderPass"         , [t| Vulkan.RenderPass          |] )
+  , ( C.TypeName "VkResult"             , [t| Vulkan.Result              |] )
+  , ( C.TypeName "VkSampleCountFlagBits", [t| Vulkan.SampleCountFlagBits |] )
+  ]
+
+vulkanCtx :: C.Context
+vulkanCtx = mempty { C.ctxTypesTable = vulkanTypesTable }
