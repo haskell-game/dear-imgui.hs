0141a84d414269f884189ff01ce517cfb59c4b9a
o1lo01ol1o <tim.pierson@gmail.com>
2021-01-25 11:47:20 +0000

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,23 @@
+let
+  hsPkgs = import ./default.nix {};
+in
+  hsPkgs.shellFor {
+    # Include only the *local* packages of your project.
+    packages = ps: with ps; [
+      dear-imgui
+    ];
+
+    # Builds a Hoogle documentation index of all dependencies,
+    # and provides a "hoogle" command to search the index.
+    withHoogle = true;
+
+    # You might want some extra tools in the shell (optional).
+
+    # Some common tools can be added with the `tools` argument
+    tools = { cabal = "3.2.0.0"; hlint = "2.2.11"; };
+    # See overlays/tools.nix for more details
+
+    # Prevents cabal from choosing alternate plans, so that
+    # *all* dependencies are provided by Nix.
+    exactDeps = true;
+  }
