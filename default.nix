0f9d665421cda04cc6de104f297eb14de913b5a0
o1lo01ol1o <tim.pierson@gmail.com>
2021-01-25 10:57:52 +0000

    add niv sources and use them

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 { # Fetch the latest haskell.nix and import its default.nix
-  haskellNix ? import (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz") {}
+  haskellNix ? (import (import ./nix/sources.nix)."haskell.nix" {})
 
 # haskell.nix provides access to the nixpkgs pins which are used by our CI,
 # hence you will be more likely to get cache hits when using these.
 # But you can also just use your own, e.g. '<nixpkgs>'.
-, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
 
 # haskell.nix provides some arguments to be passed to nixpkgs, including some
 # patches and also the haskell.nix functionality itself as an overlay.
