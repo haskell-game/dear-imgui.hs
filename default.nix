7ffa180ad89bfbe58179df7c468c78de4b6f4ebb
Tim Pierson <o1lo01ol1o@users.noreply.github.com>
2021-01-25 16:47:56 +0000

    Add Haskell.nix and use it to provide a shell.nix (#3)

---
+++ default.nix
@@ -0,0 +1,23 @@
+{ # Fetch haskell.nix and import its default.nix
+haskellNix ? (import (import ./nix/sources.nix)."haskell.nix" { })
+
+# haskell.nix provides access to the nixpkgs pins which are used by our CI,
+# hence you will be more likely to get cache hits when using these.
+# But you can also just use your own, e.g. '<nixpkgs>'.
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
+
+  # haskell.nix provides some arguments to be passed to nixpkgs, including some
+  # patches and also the haskell.nix functionality itself as an overlay.
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+, compiler-nix-name ? "ghc884"
+}:
+let
+  pkgs = import nixpkgsSrc nixpkgsArgs;
+in pkgs.haskell-nix.project {
+  inherit compiler-nix-name;
+  # 'cleanGit' cleans a source directory based on the files known by git
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "dear-imgui";
+    src = ./.;
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  hsPkgs = import ./default.nix {};
+in
+  hsPkgs.shellFor {
+    # Builds a Hoogle documentation index of all dependencies,
+    # and provides a "hoogle" command to search the index.
+    withHoogle = true;
+
+    # You might want some extra tools in the shell (optional).
+
+    # Some common tools can be added with the `tools` argument
+    tools = { cabal = "3.2.0.0"; };
+
+    # Prevents cabal from choosing alternate plans, so that
+    # *all* dependencies are provided by Nix.
+    exactDeps = true;
+  }
