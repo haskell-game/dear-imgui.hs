8b88b10c78d1df7df5ec2a2d0ae513721eb439bc
sheaf <sam.derbyshire@gmail.com>
2021-02-06 00:57:57 +0100

    Add tab bar functions

--- src/DearImGui/Context.hs
+++ src/DearImGui/Context.hs
@@ -26,12 +26,14 @@ import DearImGui.Structs
 imguiContext :: Context
 imguiContext = mempty
   { ctxTypesTable = Map.fromList
-      [ ( TypeName "ImGuiCol"  , [t| ImGuiCol |] )
-      , ( TypeName "ImGuiCond" , [t| ImGuiCond |] )
-      , ( TypeName "ImGuiDir"  , [t| ImGuiDir |] )
-      , ( TypeName "ImGuiStyleVar", [t| ImGuiStyleVar |] )
-      , ( TypeName "ImVec2"    , [t| ImVec2 |] )
-      , ( TypeName "ImVec3"    , [t| ImVec3 |] )
-      , ( TypeName "ImVec4"    , [t| ImVec4 |] )
+      [ ( TypeName "ImGuiCol" , [t| ImGuiCol |] )
+      , ( TypeName "ImGuiCond", [t| ImGuiCond |] )
+      , ( TypeName "ImGuiDir" , [t| ImGuiDir |] )
+      , ( TypeName "ImGuiStyleVar"    , [t| ImGuiStyleVar |] )
+      , ( TypeName "ImGuiTabBarFlags" , [t| ImGuiTabBarFlags |] )
+      , ( TypeName "ImGuiTabItemFlags", [t| ImGuiTabItemFlags |] )
+      , ( TypeName "ImVec2", [t| ImVec2 |] )
+      , ( TypeName "ImVec3", [t| ImVec3 |] )
+      , ( TypeName "ImVec4", [t| ImVec4 |] )
       ]
   }
