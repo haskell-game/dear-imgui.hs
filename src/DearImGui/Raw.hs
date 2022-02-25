b837d583a5a488b2691cca11cc4ac6fc4b202dc2
Stefan Dresselhaus <sdressel@techfak.uni-bielefeld.de>
2022-02-25 18:28:53 +0100

    added openPopupOnItemClick (#133)

--- src/DearImGui/Raw.hs
+++ src/DearImGui/Raw.hs
@@ -197,6 +197,7 @@ module DearImGui.Raw
   , beginPopupModal
   , endPopup
   , openPopup
+  , openPopupOnItemClick
   , closeCurrentPopup
 
     -- * ID stack/scopes
@@ -1251,6 +1252,13 @@ endPopup = liftIO do
 openPopup :: (MonadIO m) => CString -> m ()
 openPopup popupIdPtr = liftIO do
   [C.exp| void { OpenPopup($(char* popupIdPtr)) } |]
+
+-- | Call to mark popup as open (don't call every frame!).
+--
+-- Wraps @ImGui::OpenPopupOnItemClick()@
+openPopupOnItemClick :: (MonadIO m) => CString -> ImGuiPopupFlags-> m ()
+openPopupOnItemClick popupIdPtr flags = liftIO do
+  [C.exp| void { OpenPopupOnItemClick($(char* popupIdPtr), $(ImGuiPopupFlags flags)) } |]
 
 
 -- | Manually close the popup we have begin-ed into.
