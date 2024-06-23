92a3cc53ab6eed5a5d38319bca9bf8d07b18d8e8
Alexander Bondarenko <486682+dpwiz@users.noreply.github.com>
2024-06-23 22:42:43 +0300

    Bump haskell.nix (#201)
    * Bump haskell.nix
    * Bump cachix actions
    * Bump nixpkgs to 2405

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ haskellNix ? (import (import ./nix/sources.nix)."haskell.nix" { })
 # haskell.nix provides access to the nixpkgs pins which are used by our CI,
 # hence you will be more likely to get cache hits when using these.
 # But you can also just use your own, e.g. '<nixpkgs>'.
-, nixpkgsSrc ? haskellNix.sources.nixpkgs-2105
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2305
 
   # haskell.nix provides some arguments to be passed to nixpkgs, including some
   # patches and also the haskell.nix functionality itself as an overlay.
